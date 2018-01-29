1fa023c24b3835074a5fd2d438a8d3bc1fa805be
namanjain10 <namanjain10@ymail.com>
2018-01-29 23:41:15 +0530

    test4

--- firstProject/settings/production.py
+++ firstProject/settings/production.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static-serve/'
+STATIC_URL = '/home/ubuntu/Dev/cfehome/src/static-serve/'
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)),"static-serve")
 STATICFILES_DIRS = [
     STATIC_DIR,
