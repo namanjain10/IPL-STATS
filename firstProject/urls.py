566d457589d034bf82fb6f6d4e49c9d63b046b01
namanjain10 <namanjain10@ymail.com>
2018-01-29 23:50:37 +0530

    url static created

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from firstApp import views
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -42,4 +44,6 @@ urlpatterns = [
     url(r'^test/', views.testApiView)
 ]
 
+if settings.DEBUG :
+    urlpatterns.append(static(settings.STATIC_URL, document_root = settings.STATIC_ROOT))
 # urlpatterns = format_suffix_patterns(urlpatterns)
